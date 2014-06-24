37a98026a2dfaaea5383166cd17f39cfd8cadf8c
edward-ij <edward-ij@users.noreply.github.com>
2014-06-24 22:03:14 +0100

    Create serialrcvdebug.py
    Version of "serialrcv.py" including lines helpful for debugging

---
+++ serialrcvdebug.py
@@ -0,0 +1,14 @@
+from serial import Serial
+import subprocess
+
+port = Serial("/dev/ttyAMA0",9600,timeout=2)
+process = subprocess.Popen("./gpiopwm", stdin=subprocess.PIPE)
+# process = subprocess.Popen("./gpiopwm", stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+#thrust = 01500
+while True:
+    line = port.readline()
+#    process.stdin.write(str(thrust)+"\n")
+    process.stdin.write(line)
+        print line + "bytes written"
+#    for line in iter(.stdout.readline, ''):
+#        print line,
