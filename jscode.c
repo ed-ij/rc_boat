3bb0c0b7c45d42f15c5218c740455a71128b97ea
edward-ij <edward-ij@users.noreply.github.com>
2014-06-25 08:51:33 +0100

    Update jscode.c

--- jscode.c
+++ jscode.c
@@ -20,7 +20,7 @@ int open_joystick(int fd) {                             // function to open the 
     return fd;                                                   // functions like to return something
 }
 
-void read_joystick_axis(int fd, struct js_event js, int thrust, int js_bv, float js_d, int js_m, int rudder) {    // another function, including the integer fd
+void read_joystick_axis(int fd, struct js_event js, int thrust, int js_range, float js_d, int js_m, int rudder) {    // another function, including the integer fd
                                                                               // and the instance js from struct js_event
 
     while (read (fd, &js, sizeof(js)) > 0) {                    // while there is a joystick event to read
@@ -33,14 +33,14 @@ void read_joystick_axis(int fd, struct js_event js, int thrust, int js_bv, float
         else if (js.type == JS_EVENT_AXIS && js.number == 2) {       // and if that event is an axis event on the right axis */
             // printf ("Rudder Reading: %8hd\n", js.value          // print the values of it if uncommented
             rudder = -(js.value);
-            rudder = ((rudder+js_bv)/js_d)*js_m;               // variables used to make this value out of 2000
+            rudder = ((rudder+js_range)/js_d)*js_m;               // variables used to make this value out of 2000
             printf ("1%d\n", rudder);
         }
     }
 }
 
-void js_maths(int js_bv, float js_d, int js_m) {            // giving the variables for the maths their values
-    js_bv = 32767;
+void js_maths(int js_range, float js_d, int js_m) {            // giving the variables for the maths their values
+    js_range = 32767;                                          // joystick base value
     js_d = 65.534;
     js_m = 2;
 }
@@ -50,13 +50,13 @@ int main() {
     struct js_event js;
     int thrust;
     int rudder;
-    int js_bv;
+    int js_range;
     float js_d;
     int js_m;
-    js_maths(js_bv, js_d, js_m);
+    js_maths(js_range, js_d, js_m);
     fd = open_joystick(fd);
     while (1) {
-        read_joystick_axis(fd, js, thrust, js_bv, js_d, js_m, rudder);
+        read_joystick_axis(fd, js, thrust, js_range, js_d, js_m, rudder);
         fflush(stdout);
     }
     return 0;
